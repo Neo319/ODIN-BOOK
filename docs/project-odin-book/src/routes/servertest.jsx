b60bce1dc151392db54ad40dec07246f6c5988a9
CastorDisaster <alexmnevins@gmail.com>
2024-12-12 09:56:42 -0500

    create base frontend server test route

--- docs/project-odin-book/src/routes/servertest.jsx
+++ docs/project-odin-book/src/routes/servertest.jsx
@@ -1,3 +1,32 @@
-export default function serverTest() {
-  return <h1>server test lives here</h1>;
+import { useState } from "react";
+
+export default function ServerTest() {
+  const [loading, setLoading] = useState(true);
+  const [status, setStatus] = useState(true);
+
+  const URL = import.meta.env.VITE_API_URL;
+
+  async function runTest() {
+    const res = await fetch(`${URL}/servertest`);
+
+    if (res.body.success === true) {
+      setLoading(false);
+      setStatus(res.body.status);
+    } else {
+      setLoading(false);
+      setStatus("Server error!");
+    }
+  }
+
+  runTest();
+  return (
+    <>
+      <h1>server test lives here</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>Server Status: {JSON.stringify(status)}</p>
+      )}
+    </>
+  );
 }
