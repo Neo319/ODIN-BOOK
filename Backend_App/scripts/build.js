97d78240980673e489fbca8895bab8a8eeaa6b27
CastorDisaster <alexmnevins@gmail.com>
2024-12-17 14:12:16 -0500

    create one post in build script

--- Backend_App/scripts/build.js
+++ Backend_App/scripts/build.js
@@ -16,7 +16,6 @@ const prisma = (() => {
 })();
 
 // important TODO: reset prisma database here.
-
 console.log("resetting database...");
 execSync("npx prisma migrate reset").then(() => {
   console.log("database reset complete.");
@@ -26,13 +25,16 @@ execSync("npx prisma migrate reset").then(() => {
 const dbSeed = (async () => {
   console.log("creating random users...");
   try {
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 15; i++) {
       // create 15 users
       await prisma.user.create({
         data: {
           id: faker.string.uuid(),
           username: faker.internet.username(),
           password: faker.internet.password(),
+
+          // TODO
+          // avatar: faker.image.avatar(),  -- requires different schema
 
           createdAt: faker.date.past(),
         },
@@ -41,7 +43,12 @@ const dbSeed = (async () => {
       // create 15 posts
       // include: id, content, creatorId(random select..?), (no comments or likes yet)
 
-      // first ... create one post
+      // first ... create one post?
+      await prisma.post.create({
+        id: faker.string.uuid(),
+        content: faker.hacker.phrase(),
+        // creatorId: ???
+      });
     }
   } catch (err) {
     console.error(err.message);
