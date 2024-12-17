36b5bb4bdf2296dd41acf1dbfb8e477dca2c6118
CastorDisaster <alexmnevins@gmail.com>
2024-12-17 14:29:12 -0500

    fully implement new build script with posts

--- Backend_App/scripts/build.js
+++ Backend_App/scripts/build.js
@@ -15,21 +15,32 @@ const prisma = (() => {
   });
 })();
 
-// important TODO: reset prisma database here.
-console.log("resetting database...");
-execSync("npx prisma migrate reset").then(() => {
-  console.log("database reset complete.");
-});
+// storing information for later
+let userIdList = [];
 
-// creating 5 random users
+// 1. reset any existing database
+try {
+  // important TODO: reset prisma database here.
+  console.log("resetting database...");
+  const result = execSync("npx prisma migrate reset --force");
+  console.log(result);
+  console.log("database reset complete.");
+} catch (err) {
+  console.error("error resetting db - ", err.message);
+  return false;
+}
+
+// 2. seed database with sample data
 const dbSeed = (async () => {
   console.log("creating random users...");
   try {
     for (i = 0; i < 15; i++) {
+      const id = faker.string.uuid();
+
       // create 15 users
       await prisma.user.create({
         data: {
-          id: faker.string.uuid(),
+          id: id,
           username: faker.internet.username(),
           password: faker.internet.password(),
 
@@ -39,21 +50,30 @@ const dbSeed = (async () => {
           createdAt: faker.date.past(),
         },
       });
+      userIdList.push(id);
+    }
+    console.log("created users. Creating new post...");
 
-      // create 15 posts
-      // include: id, content, creatorId(random select..?), (no comments or likes yet)
+    // create 15 posts
 
-      // first ... create one post?
+    // first ... create one post?
+    for (i = 0; i < 15; i++) {
+      // determine poster id randomly
+
+      const posterId = userIdList[Math.floor(Math.random() * 15)];
+      console.log("debug, posterId= ", posterId);
+
       await prisma.post.create({
-        id: faker.string.uuid(),
-        content: faker.hacker.phrase(),
-        // creatorId: ???
+        data: {
+          id: faker.string.uuid(),
+          content: faker.hacker.phrase(),
+          creatorId: posterId,
+        },
       });
     }
   } catch (err) {
     console.error(err.message);
     return false;
   }
+  console.log("done.");
 })();
-
-console.log("done.");
