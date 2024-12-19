ee619e7bd4faaf976d58901b7f69df44bf729be3
CastorDisaster <alexmnevins@gmail.com>
2024-12-19 12:54:32 -0500

    add base for components PostForm and Detail

---
+++ docs/project-odin-book/src/components/Details.jsx
@@ -0,0 +1,10 @@
+// renders details of a user, post, etc.
+
+// content: Object with fields to render (prop key and values are rendered)
+// isEditable: boolean denoting whether this content should be editable.
+// (adds button that sends user to form for updates.)
+export default function PostForm(content, isEditable) {
+  console.log("not implemented: PostForm");
+  console.log(content);
+  console.log(isEditable);
+}
---
+++ docs/project-odin-book/src/components/PostForm.jsx
@@ -0,0 +1,11 @@
+// creates a form for uploading posts, comments, etc.
+
+// content: object containing fields to render in this form.
+// (fields should be objects with props: type, name, value.)
+
+// route: the URL this form submits to.
+export default function PostForm(content, route) {
+  console.log("not implemented: PostForm");
+  console.log(content);
+  console.log(route);
+}
